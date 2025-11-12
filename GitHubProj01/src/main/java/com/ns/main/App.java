5ba994872494e7a506a9eecc0710e82a7c0f95c8
s.l.n.suri <s.l.n.suri@ASURA1922>
2025-11-12 16:42:52 +0530

    TL:commit

---
+++ GitHubProj01/src/main/java/com/ns/main/App.java
@@ -0,0 +1,17 @@
+package com.ns.main;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+    // ✅ Correct method definition
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("1234");
+    }
+}
---
+++ GitHubProj01/src/test/java/com/ns/main/AppTest.java
@@ -0,0 +1,44 @@
+package com.ns.main;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    public void testSumWithPositives() {
+        int a = 10;
+        int b = 20;
+        int expected = 30;
+
+        App app = new App();
+        int actual = app.sum(a, b);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSumWithNegatives() {
+        int a = -10;
+        int b = -20;
+        int expected = -30;
+
+        App app = new App();
+        int actual = app.sum(a, b);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSumWithZeros() {
+        int a = 0;
+        int b = 0;
+        int expected = 0;
+
+        App app = new App();
+        int actual = app.sum(a, b);
+
+        assertEquals(expected, actual);
+    }
+}
